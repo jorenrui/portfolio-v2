f31bce5f6877626868980401069ca7e5552d7ce7
Joeylene Rivera <joeylenerivera@gmail.com>
2019-11-13 02:19:29 +0800

    Add Works Data

---
+++ src/data/worksData.js
@@ -0,0 +1,22 @@
+const worksData = [
+  {
+    id: 1,
+    title: 'SPCC Caloocan Portal',
+    description:
+      'Ut sunt sunt commodo consectetur incididunt deserunt mollit anim velit laborum sit commodo labore. Magna magna incididunt commodo ea elit elit elit Lorem veniam laboris dolore deserunt aliqua.',
+    techs: 'Boostrap, jQuery, Laravel, etc.',
+    link: 'https://spcc.tech',
+    image: 'spcc-portal.png'
+  },
+  {
+    id: 2,
+    title: 'React Portfolio',
+    description:
+      'Ut sunt sunt commodo consectetur incididunt deserunt mollit anim velit laborum sit commodo labore. Magna magna incididunt commodo ea elit elit elit Lorem veniam laboris dolore deserunt aliqua.',
+    techs: 'Flexbox, CSS Grid, React',
+    link: 'https://jorenrui.com',
+    image: 'react-portfolio.png'
+  }
+];
+
+export default worksData;
