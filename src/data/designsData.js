4130bac5cceafc005124daeca3919cf6bfff7d5c
Joeylene Rivera <joeylenerivera@gmail.com>
2019-11-14 19:59:26 +0800

    Add Design Data

---
+++ src/data/designsData.js
@@ -0,0 +1,34 @@
+const designsData = [
+  {
+    id: 1,
+    title: 'Github Mobile App Design',
+    image: 'github-mobile-app.png'
+  },
+  {
+    id: 2,
+    title: 'King Hayman Vector Art',
+    image: 'crown-hayman.png'
+  },
+  {
+    id: 3,
+    title: 'Google Map Mobile App (Night Mode)',
+    image: 'google-map-app.png'
+  },
+  {
+    id: 4,
+    title: 'Fire Slime Vector Art',
+    image: 'fire-slime.png'
+  },
+  {
+    id: 5,
+    title: 'Profile Card UI Design',
+    image: 'profile-card.png'
+  },
+  {
+    id: 6,
+    title: 'Keyboard Vector Art',
+    image: 'keyboard.png'
+  }
+];
+
+export default designsData;
