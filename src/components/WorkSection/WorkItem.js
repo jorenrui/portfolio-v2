7cc8e72a2c30c392d7877433c3b52e1c82647da4
Joeylene Rivera <joeylenerivera@gmail.com>
2020-01-03 12:07:45 +0800

    Update Works Section

--- src/components/WorkSection/WorkItem.js
+++ src/components/WorkSection/WorkItem.js
@@ -1,51 +1,50 @@
 import React from 'react';
 
 function WorkItem(props) {
-	let auth_credentials = null;
-	if (props.auth !== '') {
-		auth_credentials = (
-			<div>
-				<h4 className="mt-1">Login Credentials (For Demo)</h4>
-				<p>{props.auth}</p>
-			</div>
-		);
-	}
+  let auth_credentials = null;
+  if (props.auth !== '') {
+    auth_credentials = (
+      <div>
+        <h4 className='mt-1'>Login Credentials (For Demo)</h4>
+        <p>{props.auth}</p>
+      </div>
+    );
+  }
 
-	return (
-		<div className="work-item">
-			<div className="image">
-				<img
-					src={process.env.PUBLIC_URL + '/img/works/' + props.image}
-					alt="SPCC Caloocan Portal Website"
-				/>
-			</div>
-			<div className="info">
-				<p className="text-muted">RECENT WORK</p>
-				<h2 className="mt-0 highlight-hover">{props.title}</h2>
-				<p>{props.description}</p>
-				{auth_credentials}
-				<h4 className="mt-1">Technologies Used</h4>
-				<p>{props.techs}</p>
-				{ props.link !== '' ?
-					(<a
-						target="_blank"
-						href={props.link}
-						rel="noopener noreferrer"
-						className="btn btn-md btn-primary"
-					>
-						VIEW DEMO
-					</a>) : '' }
-				<a
-					target="_blank"
-					href={props.repo}
-					rel="noopener noreferrer"
-					className="btn btn-md"
-				>
-					VIEW REPO
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className='work-item'>
+      <div className='image'>
+        <img
+          src={process.env.PUBLIC_URL + '/img/works/' + props.image}
+          alt='SPCC Caloocan Portal Website'
+          className={props.className}
+        />
+      </div>
+      <div className='info'>
+        <p className='text-muted'>RECENT WORK</p>
+        <h2 className='mt-0 highlight-hover'>{props.title}</h2>
+        <p>{props.description}</p>
+        {auth_credentials}
+        <h4 className='mt-1'>Technologies Used</h4>
+        <p>{props.techs}</p>
+        {props.link !== '' ? (
+          <a
+            target='_blank'
+            href={props.link}
+            rel='noopener noreferrer'
+            className='btn btn-md btn-primary'
+          >
+            VIEW DEMO
+          </a>
+        ) : (
+          ''
+        )}
+        <a target='_blank' href={props.repo} rel='noopener noreferrer' className='btn btn-md'>
+          VIEW REPO
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default WorkItem;
--- src/data/worksData.js
+++ src/data/worksData.js
@@ -3,23 +3,25 @@ const worksData = [
     id: 1,
     title: 'SPCC Caloocan Portal',
     description:
-      'SPCC Caloocan Portal is a Laravel application that focuses on the grading system and the school website. It also has features like class scheduling, course enlistment, printing of grades, enrollment of students, curriculum builder, etc. Did some UI/UX Design, Database Design, and Front & Back-end Development. Case Study coming on Jan 2020.',
+      'SPCC Caloocan Portal is a Laravel application that focuses on the grading system and the school website. It also has features like class scheduling, course enlistment, printing of grades, enrollment of students, curriculum builder, etc. Did some UI/UX Design, Database Design, and Front & Back-end Development. Case Study coming soon.',
     auth: '',
     techs: 'Boostrap, jQuery, Laravel, etc.',
     link: '',
     image: 'spcc-portal.png',
-    repo: 'https://github.com/jorenrui/spccweb'
+    repo: 'https://github.com/jorenrui/spccweb',
+    className: ''
   },
   {
     id: 2,
     title: 'React Portfolio',
     description:
-      'A personal portfolio built as a way to learn and master ReactJS. Developed the design using Figma, developed the layout using Flexbox and CSS Grid, and used ReactJS. I also plan to write some case studies about my projects by Jan 2020, so stay tuned.',
+      'A personal portfolio built as a way to learn and master ReactJS. Developed the design using Figma, developed the layout using Flexbox and CSS Grid, and used ReactJS. I also plan to write some case studies about my projects, so stay tuned.',
     auth: '',
-    techs: 'React',
+    techs: 'ReactJS',
     link: '',
     image: 'react-portfolio.png',
-    repo: 'https://github.com/jorenrui/portfolio'
+    repo: 'https://github.com/jorenrui/portfolio',
+    className: 'no-shadow'
   }
 ];
 
